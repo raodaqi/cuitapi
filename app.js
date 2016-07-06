d4dff02e95d763722832b177e515a690562039e9
raodaqi <raodaqi@qq.com>
2016-07-06 16:21:29 +0800

    create 教务处 公告

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var todos = require('./routes/todos');
 var classtb = require('./routes/classtb');
+var grade = require('./routes/grade');
+var anment = require('./routes/anment');
 var cloud = require('./cloud');
 
 var app = express();
@@ -67,6 +69,8 @@ app.post('/test', function(req, res) {
 // 可以将一类的路由单独保存在一个文件中
 app.use('/todos', todos);
 app.use('/classtb', classtb);
+app.use('/grade', grade);
+app.use('/anment', anment);
 
 // 如果任何路由都没匹配到，则认为 404
 // 生成一个异常让后面的 err handler 捕获
