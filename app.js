9cbc56c50aa42d9179f45bfedd2725976099f523
raodaqi <raodaqi@qq.com>
2016-11-24 20:07:14 +0800

    修复bug

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/', function(req, res) {
 });
 
 //设置跨域访问
-app.all('*', function(req, res, next) {
+app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
